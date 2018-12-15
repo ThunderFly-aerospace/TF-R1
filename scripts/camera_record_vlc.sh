a665b865626bae0ea264a31b83286de7f29d79ad
Roman Dvořák <roman-dvorak@email.cz>
2018-12-16 00:41:04 +0100

    new script for recording with VLC

---
+++ scripts/camera_record_vlc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+station='left-front-cam'
+camera='10.42.0.122'
+today=`date '+%Y_%m_%d_%H%M%S%N'`;
+filename="./$station-$today.mkv"
+
+ # mpv --record-file=$filename "rtsp://10.42.0.67/user=admin&password=&channel=1&stream=0" --fps 19 --no-correct-pts
+
+echo "$camera"
+
+vlc "rtsp://@$camera/user=admin&password=&channel=0&stream=0" --network-caching=45 &
+cvlc "rtsp://@$camera/user=admin&password=&channel=0&stream=0" --sout "#transcode{acodec=none}:file{dst=$filename}"
+
