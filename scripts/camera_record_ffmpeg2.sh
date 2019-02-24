13e7fcdbfcea417672c5c5109bd82b1f17a822f9
Roman Dvořák <romandvorak@mlab.cz>
2019-02-24 13:36:00 +0100

    fps na kameře změněno na 20fps.

--- scripts/camera_record_ffmpeg2.sh
+++ scripts/camera_record_ffmpeg2.sh
@@ -6,4 +6,4 @@ today=`date '+%Y_%m_%d-%H_%M_%S-%N'`;
 filename="./$station-$today.mp4"
 echo "$camera"
 
-ffmpeg -i "rtsp://@$camera/user=admin&password=&channel=0&stream=0" -y $filename -movflags faststart -vcodec copy -r 19 -threads 4
+ffmpeg -i "rtsp://@$camera/user=admin&password=&channel=0&stream=0" -y $filename -movflags faststart -vcodec copy -r 20 -threads 4
