b54f10161b0cdf8d44b474e0b881bab83c569ff6
Roman Dvořák <romandvorak@mlab.cz>
2019-06-30 15:35:15 +0200

    upraveny skript na nahravani

--- scripts/IPcam/camera_record_vlc.sh
+++ scripts/IPcam/camera_record_vlc.sh
@@ -3,8 +3,10 @@
 station='left-front-cam'
 camera='10.42.0.66'
 today=`date '+%Y_%m_%d_%H%M%S%N'`;
-filename="./$station-$today.mp4"
+filename="$station-$today.mp4"
 
 echo "$camera"
 
-cvlc "rtsp://@$camera/user=admin&password=&channel=0&stream=0" --network-caching=45 --http-reconnect --sout "std{access=file{no-append,no-format,no-overwrite},mux='mp4',dst='../../cam_out/1/$filename'}"
+#rtsp://10.42.0.66/user=admin&password=&channel=1&stream=0
+
+cvlc "rtsp://@$camera/user=admin&password=&channel=1&stream=0" --sout "#std{access=file{no-append,no-format,no-overwrite},mux='mp4',dst='../../../cam_out/1/$filename'}"
--- scripts/IPcam/camera_record_vlc2.sh
+++ scripts/IPcam/camera_record_vlc2.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-station='left-front-cam'
-camera='10.42.0.66'
+station='rear-cam'
+camera='10.42.0.122'
 today=`date '+%Y_%m_%d_%H%M%S%N'`;
-filename="./$station-$today.mp4"
+filename="$station-$today.mp4"
 
 echo "$camera"
 
-cvlc "rtsp://@$camera/user=admin&password=&channel=0&stream=0" --network-caching=45 --http-reconnect --sout "std{access=file{no-append,no-format,no-overwrite},mux='mp4',dst='../../cam_out/1/$filename'}"
+#rtsp://10.42.0.66/user=admin&password=&channel=1&stream=0
+
+cvlc "rtsp://@$camera/user=admin&password=&channel=1&stream=0" --sout "#std{access=file{no-append,no-format,no-overwrite},mux='mp4',dst='../../../cam_out/1/$filename'}"
