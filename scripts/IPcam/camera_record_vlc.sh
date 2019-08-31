97ce541c9ae0e6f4c0550c2f34f786774a5610fa
Roman Dvořák <romandvorak@mlab.cz>
2019-08-31 14:49:55 +0200

    nahravani zvuku

--- scripts/IPcam/camera_record_vlc.sh
+++ scripts/IPcam/camera_record_vlc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 station='left-front-cam'
-camera='10.42.0.67'
+camera='10.42.0.66'
 today=`date '+%Y_%m_%d_%H%M%S%N'`;
 filename="$station-$today.mp4"
 
@@ -9,4 +9,4 @@ echo "$camera"
 
 #rtsp://10.42.0.66/user=admin&password=&channel=1&stream=0
 
-cvlc "rtsp://@$camera/user=admin&password=&channel=1&stream=0" --sout "#std{access=file{no-append,no-format,no-overwrite},mux='mp4',dst='../../../cam_out/1/$filename'}"
+cvlc "rtsp://@$camera/user=admin&password=&channel=1&stream=0" --sout "#std{access=file{no-append,no-format,no-overwrite},mux='mp4',dst='/data/cam_out/$filename'}"
--- scripts/IPcam/camera_record_vlc2.sh
+++ scripts/IPcam/camera_record_vlc2.sh
@@ -4,9 +4,12 @@ station='rear-cam'
 camera='10.42.0.123'
 today=`date '+%Y_%m_%d_%H%M%S%N'`;
 filename="$station-$today.mp4"
+filename = "aaa"
 
 echo "$camera"
-
 #rtsp://10.42.0.66/user=admin&password=&channel=1&stream=0
 
-cvlc "rtsp://@$camera/user=admin&password=&channel=1&stream=0" --sout "#std{access=file{no-append,no-format,no-overwrite},mux='mp4',dst='../../../cam_out/1/$filename'}"
+#cvlc "rtsp://@$camera/user=admin&password=&channel=1&stream=0" --sout "#std{access=file{no-append,no-format,no-overwrite},mux='mp4',dst='/data/cam_out/$filename'}"
+
+cvlc "rtsp://@$camera/user=admin&password=&channel=1&stream=0" --sout "#record{dst-prefix='/data/cam_out/$filename',mux='mp4'}"
+
